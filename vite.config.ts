b5633054afbfb33ef5a9cb7d753bc99a21cc414e
WilliamFitzjohn <william_fitzjohn@icloud.com>
2025-12-13 13:08:08 -0500

    updated domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GoingDuctless/', // Replace with your repo name if different
+  base: '/', // Replace with your repo name if different
   server: {
     allowedHosts: [
       'going.fitzjohn.dev',
